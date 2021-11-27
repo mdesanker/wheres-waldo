fbf0817d906cb27ea8d345ecadfc2755582ff218
Michael Desanker <mdesanker@gmail.com>
2021-11-27 10:53:38 -0500

    feat: update favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,37 @@
 import styled from "styled-components";
 
 const Header = () => {
-  return <HeaderContainer></HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <Logo>
+        <span>Where's</span> Waldo?
+      </Logo>
+    </HeaderContainer>
+  );
 };
+
+const Logo = styled.h1`
+  font-size: 1rem;
+  font-weight: 700;
+  color: red;
+  text-transform: uppercase;
+  letter-spacing: 0.05rem;
+
+  & span {
+    color: blue;
+  }
+`;
 
 const HeaderContainer = styled.header`
   position: fixed;
   top: 0;
   width: 100%;
-  height: 100px;
+  height: 50px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   border: 1px solid red;
 `;
 
