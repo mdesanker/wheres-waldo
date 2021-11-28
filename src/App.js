dc5a95303f35221e3fb6558ec517c72e8b4ddc5f
Michael Desanker <mdesanker@gmail.com>
2021-11-28 06:09:28 -0500

    feat: set up react router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/home/Home";
 import GlobalStyle from "./themes/GlobalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Fragment>
       <GlobalStyle />
       <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/wheres-waldo" element={<Home />} />
+        </Routes>
+      </Router>
       <Footer />
     </Fragment>
   );
