8d21b7f8a6021378486057cd9671d1e39c5e5fe3
Michael Desanker <mdesanker@gmail.com>
2021-11-28 14:10:49 -0500

    feat: print click coords to console

--- src/levels/Level.js
+++ src/levels/Level.js
@@ -3,16 +3,33 @@ import styled from "styled-components";
 
 const Level = (props) => {
   const { id } = useParams();
-  console.log(id);
+  // console.log(id);
 
   // Get relevant image for level selected
   const image = require(`../images/waldo-${id}.jpg`).default;
 
-  console.log(image);
+  // console.log(image);
+
+  const clickHandler = (e) => {
+    // Get coordinates of user click
+    const userX = e.clientX;
+    const userY = e.clientY;
+    console.log(`x: ${userX}, y: ${userY}`);
+
+    // Get bounding coordinates of clicked element
+    const { left, top, right, bottom } = e.target.getBoundingClientRect();
+
+    const calcX = (userX - left) / (right - left);
+    const calcY = (userY - top) / (bottom - top);
+
+    // console.log(left, top, right, bottom);
+
+    console.log([calcX.toFixed(2), calcY.toFixed(2)]);
+  };
 
   return (
     <LevelContainer>
-      <Map src={image} />
+      <Map src={image} onClick={clickHandler} />
     </LevelContainer>
   );
 };
