c5d8315d9338f8fdf4b63fd30062205f6de6075b
Michael Desanker <mdesanker@gmail.com>
2021-11-28 11:24:31 -0500

    feat: load correct image into level component

--- src/levels/Level.js
+++ src/levels/Level.js
@@ -2,12 +2,26 @@ import { useParams } from "react-router";
 import styled from "styled-components";
 
 const Level = (props) => {
-  console.log(props);
   const { id } = useParams();
   console.log(id);
 
-  return <LevelContainer>Test</LevelContainer>;
+  // Get relevant image for level selected
+  const image = require(`../images/waldo-${id}.jpg`).default;
+
+  console.log(image);
+
+  return (
+    <LevelContainer>
+      Test
+      <Map src={image} />
+    </LevelContainer>
+  );
 };
+
+const Map = styled.img`
+  width: 400px;
+  height: 400px;
+`;
 
 const LevelContainer = styled.main`
   margin: 50px auto;
