a07d3b2e6913a17973d7e578947a904750c73622
Michael Desanker <mdesanker@gmail.com>
2021-11-29 08:16:43 -0500

    feat: style char icons

--- src/views/levels/CharacterIcon.js
+++ src/views/levels/CharacterIcon.js
@@ -1,10 +1,30 @@
+import styled from "styled-components";
+
 const CharacterIcon = (props) => {
+  const capitalize = (str) => {
+    const arr = str.toLowerCase().split("");
+    return arr[0].toUpperCase() + arr.slice(1).join("");
+  };
+
   return (
-    <div>
-      <img src={props.img} alt="" />
-      <label>{props.name}</label>
-    </div>
+    <Wrapper>
+      <IconImage src={props.img} alt="" />
+      <label>{capitalize(props.name)}</label>
+    </Wrapper>
   );
 };
+
+const IconImage = styled.img`
+  height: 40px;
+  width: 40px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 20px;
+`;
 
 export default CharacterIcon;
