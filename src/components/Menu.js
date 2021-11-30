42cfda26dc0679e7a761394c3e65d26da72d9e83
Michael Desanker <mdesanker@gmail.com>
2021-11-29 21:56:49 -0500

    feat: click on map to move menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,22 @@
 import styled from "styled-components";
 
 const Menu = (props) => {
-  return <MenuContainer></MenuContainer>;
+  console.log(props.position);
+  return <MenuContainer pos={props.position}></MenuContainer>;
 };
 
 const MenuContainer = styled.div`
   position: absolute;
-  top: 50%;
-  left: 50%;
+  top: 0;
+  left: 0;
   width: 100px;
   height: 100px;
 
   border-radius: 5px;
   border: 1px solid #e5e5e5;
   background-color: #f1f1f1;
+
+  transform: translate(${(props) => `${props.pos[0]}px, ${props.pos[1]}px`});
 `;
 
 export default Menu;
--- src/views/levels/Level.js
+++ src/views/levels/Level.js
@@ -11,7 +11,7 @@ const Level = (props) => {
   const { id } = useParams();
 
   const [level, setLevel] = useState({});
-  const [hidden, setHidden] = useState({});
+  const [hidden, setHidden] = useState(false);
 
   const [currentClick, setCurrentClick] = useState([]);
 
@@ -31,48 +31,79 @@ const Level = (props) => {
   // Get relevant image for level selected
   const image = require(`../../images/waldo-${id}.jpg`).default;
 
-  // First Gen click handler
+  // // First Gen click handler
+  // const clickHandler = (e) => {
+  //   // Get coordinates of user click
+  // const userX = e.clientX;
+  // const userY = e.clientY;
+
+  //   const map = document.querySelector(`#map-${level.id}`);
+  //   console.log(map);
+
+  //   // Get bounding coordinates of clicked element
+  //   const { left, top, right, bottom } = map.getBoundingClientRect();
+
+  // const calcX = (userX - left) / (right - left);
+  // const calcY = (userY - top) / (bottom - top);
+
+  //   // console.log(left, top, right, bottom);
+
+  //   const coords = [
+  //     Number.parseFloat(calcX.toFixed(3)),
+  //     Number.parseFloat(calcY.toFixed(3)),
+  //   ];
+
+  //   setCurrentClick(coords);
+
+  //   // Check if waldo clicked
+  //   console.log(
+  //     "waldo clicked",
+  //     Math.abs(coords[0] - hidden.waldo[0]) < 0.02 &&
+  //       Math.abs(coords[1] - hidden.waldo[1]) < 0.02
+  //   );
+
+  //   console.log(
+  //     "odlaw clicked",
+  //     Math.abs(coords[0] - hidden.odlaw[0]) < 0.02 &&
+  //       Math.abs(coords[1] - hidden.odlaw[1]) < 0.02
+  //   );
+
+  //   console.log(
+  //     "wizard clicked",
+  //     Math.abs(coords[0] - hidden.wizard[0]) < 0.02 &&
+  //       Math.abs(coords[1] - hidden.wizard[1]) < 0.02
+  //   );
+  // };
+
   const clickHandler = (e) => {
-    // Get coordinates of user click
     const userX = e.clientX;
     const userY = e.clientY;
+    console.log("click", [userX, userY]);
+
+    // const coords = [
+    //   Number.parseFloat(userX.toFixed(3)),
+    //   Number.parseFloat(userY.toFixed(3)),
+    // ];
 
     const map = document.querySelector(`#map-${level.id}`);
-    console.log(map);
-
-    // Get bounding coordinates of clicked element
     const { left, top, right, bottom } = map.getBoundingClientRect();
 
-    const calcX = (userX - left) / (right - left);
-    const calcY = (userY - top) / (bottom - top);
+    console.log("top", top);
+    console.log("left", left);
 
-    // console.log(left, top, right, bottom);
+    // const calcX = (userX - left) / (right - left);
+    // const calcY = (userY - top) / (bottom - top);
+
+    const calcX = userX - left;
+    const calcY = userY - top;
 
     const coords = [
       Number.parseFloat(calcX.toFixed(3)),
       Number.parseFloat(calcY.toFixed(3)),
     ];
+    console.log(coords);
 
     setCurrentClick(coords);
-
-    // Check if waldo clicked
-    console.log(
-      "waldo clicked",
-      Math.abs(coords[0] - hidden.waldo[0]) < 0.02 &&
-        Math.abs(coords[1] - hidden.waldo[1]) < 0.02
-    );
-
-    console.log(
-      "odlaw clicked",
-      Math.abs(coords[0] - hidden.odlaw[0]) < 0.02 &&
-        Math.abs(coords[1] - hidden.odlaw[1]) < 0.02
-    );
-
-    console.log(
-      "wizard clicked",
-      Math.abs(coords[0] - hidden.wizard[0]) < 0.02 &&
-        Math.abs(coords[1] - hidden.wizard[1]) < 0.02
-    );
   };
 
   // TODO: write function to check if objectives clicked
@@ -81,11 +112,11 @@ const Level = (props) => {
 
   // TODO: add menu showing objectives to find
 
-  console.log("click", currentClick);
+  // console.log("click", currentClick);
 
   return (
     <LevelContainer>
-      <Menu />
+      <Menu position={currentClick} />
       <Characters chars={level.objectives} />
       <Map src={image} onClick={clickHandler} id={`map-${level.id}`} />
 
