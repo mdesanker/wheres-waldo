3c3473b159ae4d4d197b33449ef21ca2bf20a065
Michael Desanker <mdesanker@gmail.com>
2021-12-03 06:45:18 -0500

    Wrap app in ctx provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import RouterSwitch from "./RouterSwitch";
+import { LevelContextProvider } from "./utils/level-context";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <LevelContextProvider>
     <RouterSwitch />
-  </React.StrictMode>,
+  </LevelContextProvider>,
   document.getElementById("root")
 );
--- src/utils/level-context.js
+++ src/utils/level-context.js
@@ -3,9 +3,7 @@ import React, { useState, useEffect, Children } from "react";
 const LevelContext = React.createContext({});
 
 export const LevelContextProvider = (props) => {
-  return (
-    <LevelContext.Provider value={{}}>{props.children}</LevelContext.Provider>
-  );
+  return <LevelContext.Provider>{props.children}</LevelContext.Provider>;
 };
 
 export default LevelContext;
