2deb0097e6fedcd10e7c561fc467e39890850a2e
Michael Desanker <mdesanker@gmail.com>
2021-12-05 06:15:49 -0500

    Add Leaderboard header

--- src/views/leaderboard/Leaderboard.js
+++ src/views/leaderboard/Leaderboard.js
@@ -1,5 +1,30 @@
+import { Fragment } from "react";
+import styled from "styled-components";
+
 const Leaderboard = () => {
-  return <h1>Leaderboard</h1>;
+  return (
+    <Fragment>
+      <LeaderboardHeader>
+        <h1>Leaderboard</h1>
+      </LeaderboardHeader>
+      <LeaderboardContainer></LeaderboardContainer>
+    </Fragment>
+  );
 };
+
+const LeaderboardContainer = styled.main``;
+
+const LeaderboardHeader = styled.header`
+  width: 100%;
+  padding: 0.5rem 1rem;
+  border-bottom: 1px solid #e5e5e5;
+  display: flex;
+  justify-content: center;
+
+  & h1 {
+    width: 100%;
+    max-width: 1200px;
+  }
+`;
 
 export default Leaderboard;
