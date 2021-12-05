ab0a66d113cd95dd5d9f8546fd4b20f823d75288
Michael Desanker <mdesanker@gmail.com>
2021-12-05 13:26:38 -0500

    Add home btn to leaderboard pg

--- src/views/leaderboard/Leaderboard.js
+++ src/views/leaderboard/Leaderboard.js
@@ -1,20 +1,20 @@
 import { Fragment, useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import LevelContext from "../../store/level-context";
 import LevelTab from "./LevelTab";
 import ScoreTable from "./ScoreTable";
+import Button from "../../components/Button";
 
 const Leaderboard = () => {
   const levelCtx = useContext(LevelContext);
-  // console.log(levelCtx);
-
   const [activeLevelID, setActiveLevelID] = useState(1);
 
   const currentLevelInfo = levelCtx.levels.find(
     (level) => level.id === activeLevelID
   );
 
-  console.log(currentLevelInfo);
+  // console.log(currentLevelInfo);
 
   const activeLevelClickHandler = (e) => {
     const { id } = e.target.closest("div");
@@ -35,7 +35,12 @@ const Leaderboard = () => {
   return (
     <Fragment>
       <LeaderboardHeader>
-        <h1>Leaderboard</h1>
+        <HeaderContainer>
+          <h1>Leaderboard</h1>
+          <Link to="/wheres-waldo">
+            <Button theme={{ color: "white", background: "blue" }}>Home</Button>
+          </Link>
+        </HeaderContainer>
       </LeaderboardHeader>
       <LeaderboardContainer>
         <TabContainer>{levels}</TabContainer>
@@ -60,17 +65,22 @@ const LeaderboardContainer = styled.main`
   margin: 0 auto;
 `;
 
+const HeaderContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const LeaderboardHeader = styled.header`
   width: 100%;
-  padding: 0.5rem 1rem;
+  padding: 0 1rem;
   border-bottom: 1px solid #e5e5e5;
   display: flex;
-  justify-content: center;
-
-  & h1 {
-    width: 100%;
-    max-width: 1200px;
-  }
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export default Leaderboard;
